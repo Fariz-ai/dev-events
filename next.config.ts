4c4fae12815183b7664f854cde342994d8170c80
Fariz <farizfadliansyah@gmail.com>
2025-11-08 19:52:09 +0700

    first commmit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
+/** @format */
+
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
